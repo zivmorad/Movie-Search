2120d3e28ceb65d6f383c1c1f726e7b6d8fe6426
moradziv <moradziv1994@gmail.com>
2020-05-19 19:51:30 +0300

    fix navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
+import {BrowserRouter,Route} from 'react-router-dom'
 
 const App=()=>{
     return(
          <div>
-             <Navbar/>
+             <BrowserRouter>
+                <div>
+                    <Navbar/>
+                    <Route path='/' exact={'#'}/>
+                    <Route path='/currentVideo' exact={'#'}/>
+                </div>
+             </BrowserRouter>
          </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 const Navbar=()=>{
     return(
         <div className='nav-container'>
-            <h3 className='logo'>Ziv</h3>
-            <h1 className='nav-title'>Cinima World</h1>
+            <Link to='/' className='logo'>Ziv</Link>
+            <Link to='/' className='nav-title'>Cinima World</Link>
             <div className='nav-icons-warp'>
-                <i class="fab fa-github fa-2x"></i>
-                <i class="fab fa-linkedin fa-2x"></i>
+                <i className="fab fa-github fa-2x" onClick={()=>{window.open('https://github.com/zivmorad/movie-search')}}></i>
+                <i className="fab fa-linkedin fa-2x" onClick={()=>{window.open('https://www.linkedin.com/in/ziv-morad-216a6a18a/')}}></i>
             </div>
         </div>)
 }
