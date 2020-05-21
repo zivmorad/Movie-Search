78a319f8426c315ccf88e30cc30f314a797f207f
moradziv <moradziv1994@gmail.com>
2020-05-21 18:10:58 +0300

    add SearchMovie to Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import {BrowserRouter,Route} from 'react-router-dom'
-
+import SearchMovie from './SearchMovie/SearchMovie'
+import './App.css'
 const App=()=>{
     return(
          <div>
              <BrowserRouter>
                 <div>
                     <Navbar/>
-                    <Route path='/' exact={'#'}/>
-                    <Route path='/currentVideo' exact={'#'}/>
+                    <Route path='/' exact component={SearchMovie}/>
+                    <Route path='/currentVideo' exact component={'#'}/>
                 </div>
              </BrowserRouter>
          </div>
