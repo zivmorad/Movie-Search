9767e8527d6ae4ffb1ac28dd9b492f6d27d9c84d
moradziv <moradziv1994@gmail.com>
2020-05-25 19:54:40 +0300

    fix components

--- src/components/SearchMovie/MovieCard/MovieCard.js
+++ src/components/SearchMovie/MovieCard/MovieCard.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './MovieCard.css'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {fetchMovie} from '../../../actions'
 
-const MovieCard=(props)=>{
-    return(
-        <div className='card-movie-container'>
-            <img className='card-movie-img' src={`https://image.tmdb.org/t/p/w185${props.img}`} alt='error'/>
-            <h5 className='card-movie-title'>{props.title}</h5>
-            <Link to='/currentVideo' className='movie-detail-btn'>View details</Link>
-        </div>
-    )
+class MovieCard extends Component{
+
+    fetchMovieById=()=>{
+        this.props.fetchMovie(this.props.id)
+    }
+
+    render(){
+        return(
+            <div className='card-movie-container'>
+                <img className='card-movie-img' src={this.props.img} alt='error'/>
+                <h5 className='card-movie-title'>{this.props.title}</h5>
+                <Link to='/currentVideo' className='movie-detail-btn' onClick={this.fetchMovieById}>View details</Link>
+            </div>
+        )
+
+    }
 }
 
-export default MovieCard;
+
+
+export default connect(null,{fetchMovie}) (MovieCard);
