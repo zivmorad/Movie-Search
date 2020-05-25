918219147341ed6b418ef6d41eba9c59a8cd8e83
moradziv <moradziv1994@gmail.com>
2020-05-25 19:53:37 +0300

    fix actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
-import TMDB from "../api/TMDB"
-import {FETCH_MOVIES} from './types'
+import axios from 'axios'
+import {FETCH_MOVIES,FETCH_MOVIE_BY_ID} from './types'
 
 export const fetchMovies = searchInput => async dispatch=>{
-    const res=await TMDB.get('/search/movie',{
-        params:{
-            api_key:'9d07c6739da38cecbd8f76c867a2dc68',
-            query:searchInput
-        }
-    })
-    dispatch({ type:FETCH_MOVIES , payload:res.data.results })
+    const res=await axios.get(`http://www.omdbapi.com?apikey=3c84f7ac&s=${searchInput}`)
+    dispatch({ type:FETCH_MOVIES , payload:res.data.Search })
+}
+
+export const fetchMovie=movieId=> async dispatch=>{
+    const res=await axios.get(`http://www.omdbapi.com?apikey=3c84f7ac&i=${movieId}`)
+    dispatch({ type:FETCH_MOVIE_BY_ID , payload:res.data })
 }
