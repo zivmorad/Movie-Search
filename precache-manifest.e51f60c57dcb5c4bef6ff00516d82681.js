c9e3a6495e662539e0ad253a6e4d658afd777f15
moradziv <moradziv1994@gmail.com>
2020-05-26 12:27:34 +0300

    Updates

---
+++ precache-manifest.e51f60c57dcb5c4bef6ff00516d82681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05038f179f97a1ecaf1ba28edfa3857",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "55b1530206df770ebdb3",
+    "url": "/movie-search/static/css/main.029f3f7e.chunk.css"
+  },
+  {
+    "revision": "699e5a67f1e65489ce6f",
+    "url": "/movie-search/static/js/2.1fda319c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search/static/js/2.1fda319c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b1530206df770ebdb3",
+    "url": "/movie-search/static/js/main.6d43759d.chunk.js"
+  },
+  {
+    "revision": "eb7c7aee420f65e28f7e",
+    "url": "/movie-search/static/js/runtime-main.8a6e7700.js"
+  },
+  {
+    "revision": "e549f9798674301c5af2c91581194091",
+    "url": "/movie-search/static/media/spiner.e549f979.gif"
+  }
+]);
