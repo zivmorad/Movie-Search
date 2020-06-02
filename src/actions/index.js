0b5adb6afa80b28000e134c7b6c2e6e3e20d887a
moradziv <moradziv1994@gmail.com>
2020-06-02 21:09:27 +0300

    fix apikey

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import {FETCH_MOVIES,FETCH_MOVIE_BY_ID} from './types'
-
+import {apiKey} from '../confing/apiKey'
 export const fetchMovies = searchInput => async dispatch=>{
-    const res=await axios.get(`http://www.omdbapi.com?apikey=3c84f7ac&s=${searchInput}`)
+    const res=await axios.get(`http://www.omdbapi.com?apikey=${apiKey}&s=${searchInput}`)
     dispatch({ type:FETCH_MOVIES , payload:res.data.Search })
 }
 
 export const fetchMovie=movieId=> async dispatch=>{
-    const res=await axios.get(`http://www.omdbapi.com?apikey=3c84f7ac&i=${movieId}`)
+    const res=await axios.get(`http://www.omdbapi.com?apikey=${apiKey}&i=${movieId}`)
     dispatch({ type:FETCH_MOVIE_BY_ID , payload:res.data })
 }
